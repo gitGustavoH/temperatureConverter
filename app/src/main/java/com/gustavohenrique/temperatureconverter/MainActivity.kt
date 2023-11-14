eadb158d4495db9eb0e97159719dfac561b4d491
Gustavo Henrique <gustavohenriquecns.wk@gmail.com>
2023-11-13 17:58:26 -0800

    The Main logic is finish, TODO somethings about interface and the fun to show the result.

--- app/src/main/java/com/gustavohenrique/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/gustavohenrique/temperatureconverter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gustavohenrique.temperatureconverter
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.core.content.res.TypedArrayUtils.getText
 import com.gustavohenrique.temperatureconverter.R.id.Celsius
@@ -11,34 +12,32 @@ import java.math.BigInteger
 import java.math.BigInteger.*
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        var celsiusTextView: TextView = findViewById(R.id.Celsius)
-        val fTextView: TextView = findViewById(R.id.r)
-        var resultado = 0.0
-
-        fun calCelsiusToF() {
+    var resultado = 0.0
+    fun calCelsiusToF(view: View) {
+            var celsiusTextView: TextView = findViewById(R.id.Celsius)
             val celsiusString = celsiusTextView.text.toString()
 
             var celsius = celsiusString.toDouble()
              resultado = (celsius * 1.8) + 32
-        }
+    }
 
-        fun calFtoCelsius() {
-            val fString = fTextView.text.toString()
+    fun calFtoCelsius(view: View) {
+        val fTextView: TextView = findViewById(R.id.r)
+        val fString = fTextView.text.toString()
 
             var f = fString.toDouble()
             resultado = (f - 32) / 1.8
+
         }
 
-
-
-
     }
-}
+
+
 
 
 
