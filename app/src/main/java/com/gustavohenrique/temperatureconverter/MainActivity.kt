2d5a26d420ca60ac4e55dce7c3f5c5acea057f8f
Gustavo Henrique <gustavohenriquecns.wk@gmail.com>
2023-11-14 15:55:40 -0800

    Project is done!

--- app/src/main/java/com/gustavohenrique/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/gustavohenrique/temperatureconverter/MainActivity.kt
@@ -7,35 +7,64 @@ import android.view.View
 import android.widget.TextView
 import androidx.core.content.res.TypedArrayUtils.getText
 import com.gustavohenrique.temperatureconverter.R.id.Celsius
+import com.gustavohenrique.temperatureconverter.R.id.textoResultado
 import java.math.BigDecimal
 import java.math.BigInteger
 import java.math.BigInteger.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textoResultado: TextView
+    var resultado = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textoResultado = findViewById<TextView>(R.id.textoResultado)
     }
 
-    var resultado = 0.0
+    fun formatDecimal(number: Double, decimalPlaces: Int): String {
+        return "%.${decimalPlaces}f".format(number)
+    }
+
+
     fun calCelsiusToF(view: View) {
-            var celsiusTextView: TextView = findViewById(R.id.Celsius)
-            val celsiusString = celsiusTextView.text.toString()
+            var celsiusTextView: TextView = findViewById(Celsius)
+            var celsiusString = celsiusTextView.text.toString()
 
-            var celsius = celsiusString.toDouble()
+        if (celsiusString.isNotEmpty()) {
+            var celsius = celsiusString.toFloat()
              resultado = (celsius * 1.8) + 32
-    }
 
-    fun calFtoCelsius(view: View) {
-        val fTextView: TextView = findViewById(R.id.r)
-        val fString = fTextView.text.toString()
+        val resultadoFormatado = formatDecimal(resultado, 2)
 
-            var f = fString.toDouble()
-            resultado = (f - 32) / 1.8
-
+        textoResultado.text = resultadoFormatado
+        } else {
+            textoResultado.text = "The box is empty"
         }
 
     }
+
+    fun calFtoCelsius(view: View) {
+        var fTextView: TextView = findViewById(R.id.r)
+        var fString: String = fTextView.text.toString()
+
+        if (fString.isNotEmpty()) {
+            var f = fString.toFloat()
+            resultado = (f - 32) / 1.8
+
+
+        val resultadoFormatado = formatDecimal(resultado, 2)
+
+        textoResultado.text = resultadoFormatado
+        } else {
+            textoResultado.text = "The box is empty"
+        }
+
+
+    }
+}
 
 
 
